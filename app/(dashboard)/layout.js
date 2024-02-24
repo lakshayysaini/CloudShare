9d1e02c1c486ac576131b357285ee5c0e6fdb7cb
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-02-24 22:10:44 +0530

    updated code logo added

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function layout( { children } ) {
     return (
-        <div>{ children }</div>
+        <div>
+            <div className='hidden h-full md:w-64 flex-col fixed inset-y-0 z-50'>
+
+            </div>
+            <div className='md:ml-64'>
+                { children }
+            </div>
+        </div>
     )
 }
 
