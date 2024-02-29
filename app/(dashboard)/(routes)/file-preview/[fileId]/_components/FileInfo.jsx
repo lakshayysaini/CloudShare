0ca853c741bb4ad0b3e276c76b4be5712e17bfcc
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-02-29 23:41:38 +0530

    updated code with logic of getting the file information completed

---
+++ app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileInfo.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const FileInfo = ( { file } ) => {
+    const [fileType, setFileType] = useState();
+    return (
+        <div>FileInfo</div>
+    )
+}
+
+export default FileInfo
