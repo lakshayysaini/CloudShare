f3de17f60004db97cfc324db94bb5fe36911a0df
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-03-04 23:58:33 +0530

    updated code with working send email with necessary information

--- app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileShareForm.jsx
+++ app/(dashboard)/(routes)/file-preview/[fileId]/_components/FileShareForm.jsx
@@ -18,7 +18,7 @@ const FileShareForm = ( { file, onPasswordSave } ) => {
             fileType: file.fileType,
             shortUrl: file.ShortUrl,
         }
-        GlobalApi.SendEmail(data).then( resp => {
+        GlobalApi.SendEmail( data ).then( resp => {
             console.log( resp );
         } )
     };
@@ -60,7 +60,7 @@ const FileShareForm = ( { file, onPasswordSave } ) => {
             <div className='border rounded-md p-2 flex flex-col mt-5'>
                 <label className='text-[14px] text-gray-500'>Send File to Email</label>
 
-                <input type='text' className='border rounded-md bg-transparent p-2 mt-3' />
+                <input type='text' className='border rounded-md bg-transparent p-2 mt-3' onChange={ ( e ) => setEmail( e.target.value ) } />
 
                 <button className='bg-primary mt-2 rounded-md p-2 text-white' onClick={ () => sendEmail() }>
                     Send Email
--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,15 +1,17 @@
 import { Resend } from "resend";
-import {EmailTemplate} from "../../_components/email-template";
+import { EmailTemplate } from "../../_components/email-template";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export async function POST() {
+export async function POST(req) {
+  const response = await req.json();
+  console.log("response", response);
   try {
     const data = await resend.emails.send({
       from: "CloudShare@resend.dev",
       to: "lakshayandsaini@gmail.com",
-      subject: "Hello world",
-      react: EmailTemplate({ firstName: "John" }),
+      subject: response?.userName + " shared file with you",
+      react: EmailTemplate({ response }),
     });
 
     return Response.json(data);
