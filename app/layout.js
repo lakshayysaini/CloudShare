88336229b6b0fb3ff9263aa02959177297c3d31c
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-03-10 00:19:54 +0530

    udpated code with completed about us section

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,6 @@ export default function RootLayout( { children } ) {
     <ClerkProvider>
       <html lang="en">
         <body className={ inter.className }>
-          <Header />
           { children }
         </body>
       </html>
