1dc4d015db61841371123cb1f2624b51cd6907ec
lakshayystrideAhead <lakshay.saini@strideahead.in>
2024-03-11 22:01:01 +0530

    updated code with all features unlocked

--- app/(dashboard)/_components/TopHeader.jsx
+++ app/(dashboard)/_components/TopHeader.jsx
@@ -7,7 +7,9 @@ function TopHeader() {
     return (
         <div className='flex p-5 border-b items-center justify-between md:justify-end'>
             <AlignJustify className='md:hidden' />
-            <Image src='/CloudShare.png' width={ 125 } height={ 50 } className='md:hidden' href='#' />
+            <a href="/">
+                <Image src='/CloudShare.png' width={ 125 } height={ 50 } className='md:hidden' />
+            </a>
             <UserButton />
         </div>
     )
